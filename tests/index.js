de9322e1b87c3b51601d5ebf210d141e4528fc91
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-04-22 16:05:40 +0000

    Add base test file

---
+++ tests/index.js
@@ -0,0 +1,8 @@
+var test = require('chai').assert;
+var index = require('../index');
+
+describe('index', function() {
+  it('should be a function', function () {
+    test.isFunction( index );
+  });
+});
